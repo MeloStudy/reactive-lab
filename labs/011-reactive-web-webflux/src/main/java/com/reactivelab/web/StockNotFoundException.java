059b50fa56ec79f7c02e3e3c4c9da8a3ed444239
MeloDev <melodiaz2111@gmail.com>
2026-05-06 12:26:53 -0500

    feat: update lab 11

--- labs/011-reactive-web-webflux/src/main/java/com/reactivelab/web/StockNotFoundException.java
+++ labs/011-reactive-web-webflux/src/main/java/com/reactivelab/web/StockNotFoundException.java
@@ -1,5 +1,9 @@
 package com.reactivelab.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class StockNotFoundException extends RuntimeException {
     public StockNotFoundException(String symbol) {
         super("Stock not found: " + symbol);
